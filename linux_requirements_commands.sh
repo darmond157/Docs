eee6f422fc719a275c9ac6a0bb150cba643197c3
darmond157 <bardiarezaeikalantari@gmail.com>
2024-04-23 11:52:26 +0330

    chore: add linux requirements bash file to install necessary files

---
+++ linux_requirements_commands.sh
@@ -0,0 +1,23 @@
+#Docker
+#Php
+#node
+#npm
+#apache-server
+#simple screen record
+#ZSH
+#xtreme download
+#postman
+#pgadmin
+#robo3t
+#postgres driver
+#chrome
+#mpv media player
+#golang
+#python
+#pip
+#jupyter
+#nvm
+#nodemon
+#http server
+#android studio
+#vscode
