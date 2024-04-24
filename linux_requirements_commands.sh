a098976824dd315897b3059c3f6eff2087385bdd
darmond157 <bardiarezaeikalantari@gmail.com>
2024-04-24 20:52:34 +0330

    update

--- linux_requirements_commands.sh
+++ linux_requirements_commands.sh
@@ -1,23 +1,46 @@
 #Docker
-#Php
-#node
-#npm
-#apache-server
-#simple screen record
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+#nodejs
+sudo apt install nodejs
+sudo apt install npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm list-remote
 #ZSH
-#xtreme download
-#postman
+sudo apt install zsh -y
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 #pgadmin
-#robo3t
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4-desktop
 #postgres driver
+sudo apt install PostgreSQL PostgreSQL-contrib -y
 #chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i ./google-chrome-stable_current_amd64.deb
 #mpv media player
 #golang
 #python
-#pip
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install --upgrade pip
+pip3 install notebook
 #jupyter
-#nvm
 #nodemon
+npm i -g nodemon
 #http server
+npm i -g http-server
 #android studio
 #vscode
+sudo snap install --classic code
+#telegram
+sudo snap install telegram-desktop
+#git
+sudo apt install git
